399178d96fab5b083f70e1820662db88a179952b
himanshu9783 <himanshugothwal.2017@gmail.com>
2025-03-25 14:39:29 +0530

    delete

--- assets/zendesk-embeddable.js
+++
@@ -1,1 +0,0 @@
-var script = document.createElement('script');script.type = 'text/javascript';script.id = 'ze-snippet';script.src = 'https://static.zdassets.com/ekr/snippet.js?key=a521d705-cec9-4342-9458-6fdac8bf097f';document.getElementsByTagName('head')[0].appendChild(script);
